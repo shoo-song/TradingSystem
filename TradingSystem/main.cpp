f9cebfdf7d909a20a3d887fa5f82273e0ae7920c
Bongsu Jeon <bs.jeon87@gmail.com>
2025-03-31 15:49:22 +0900

    Refactor

--- TradingSystem/main.cpp
+++ TradingSystem/main.cpp
@@ -1,17 +1,11 @@
 #include "gmock/gmock.h"
 #include <iostream>
-#include "NemoAPI.cpp"
-#include "KiwerAPI.cpp"
+#include "StockerBroker.cpp"
+#include "NemoDriver.cpp"
+#include "KiwerDriver.cpp"
 using namespace std;
 using namespace testing;
-class StockerBrockerInterface {
-public:
-	virtual void selectStockBrocker(bool IsNemo) = 0;
-	virtual void login(string ID, string Password) = 0;
-	virtual void buy(string stockCode, int price, int count) = 0;
-	virtual void sell(string stockCode, int price, int count) = 0;
-	virtual int getPrice(string stockCode, int minute) = 0;
-};
+
 class MockDriver : public StockerBrockerInterface {
 public:
 	MOCK_METHOD(void, selectStockBrocker, (bool IsNemo), (override));
@@ -20,32 +14,7 @@ public:
 	MOCK_METHOD(void, sell, (string stockCode, int price, int count), (override));
 	MOCK_METHOD(int, getPrice, (string stockCode, int minute), (override));
 };
-class StockerBrockerDriver {
-public:
-	StockerBrockerDriver(StockerBrockerInterface* API) {
-		m_API = API;
-	}
-	void login(string ID, string Password) {
-		m_API->login(ID, Password);
-	}
-	void buy(string stockCode, int price, int count) {
 
-	}
-	void sell(string stockCode, int price, int count) {
-
-	}
-	int getPrice(string stockCode, int minute) {
-		return 0;
-	}
-	void buyNiceTiming(string stockCode, int price) {
-
-	}
-	void sellNiceTiming(string stockCode, int count) {
-
-	}
-private:
-	StockerBrockerInterface* m_API;
-};
 class TradingFixture : public Test {
 public:
 	MockDriver Mock;
