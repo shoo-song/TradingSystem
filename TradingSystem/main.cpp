8c37779e7b308295bbb1fcc48d84235d6e51b772
shoo.song <shoo.song@samsung.com>
2025-03-31 14:40:48 +0900

    add TC

--- TradingSystem/main.cpp
+++ TradingSystem/main.cpp
@@ -1,29 +1,52 @@
 #include "gmock/gmock.h"
 #include <iostream>
+#include "NemoAPI.cpp"
+#include "KiwerAPI.cpp"
 using namespace std;
-class StockerBrocker {
+class StockerBrockerInterface {
 public:
-	virtual bool login(string ID, string Password) = 0;
+	virtual void selectStockBrocker(bool IsNemo) = 0;
+	virtual void login(string ID, string Password) = 0;
+	virtual void buy(string stockCode, int price, int count) = 0;
+	virtual void sell(string stockCode, int price, int count) = 0;
+	virtual int getPrice(string stockCode, int minute) = 0;
+};
+class MockDriver : public StockerBrockerInterface {
+public:
+	MOCK_METHOD(void, selectStockBrocker, (bool IsNemo), (override));
+	MOCK_METHOD(void, login, (string ID, string Password), (override));
+	MOCK_METHOD(void, buy, (string stockCode, int price, int count), (override));
+	MOCK_METHOD(void, sell, (string stockCode, int price, int count), (override));
+	MOCK_METHOD(int, getPrice, (string stockCode, int minute), (override));
+};
+class StockerBrockerDriver {
+public:
+	StockerBrockerDriver(StockerBrockerInterface* API) {
+		m_API = API;
+	}
+	void login(string ID, string Password) {
+
+	}
+	void buy(string stockCode, int price, int count) {
+
+	}
+	void sell(string stockCode, int price, int count) {
+
+	}
+	int getPrice(string stockCode, int minute) {
+
+	}
 private:
-	
+	StockerBrockerInterface* m_API;
 };
-class Kiwer : public StockerBrocker{
-public:
-	bool login(string ID, string Password) override{
-		//TODO
-	}
-};
-class Nemo : public StockerBrocker {
-public:
-	bool login(string ID, string Password) override{
-		//TODO
-	}
-};
-TEST(Trading, SelectTest) {
-	// selct App
-}
+
 TEST(Trading, LoginTest) {
 	// Login
+	MockDriver Mock;
+	EXPECT_CALL(Mock, login("shoo-song", "1234"))
+		.Times(1);
+	StockerBrockerDriver Driver{ &Mock };
+	Driver.login("shoo-song", "1234");
 }
 TEST(Trading, BuyTest) {
 	// Buy
