54871b108bf52d095dad7348d0713edd07f7a681
shoo.song <shoo.song@samsung.com>
2025-03-31 15:14:01 +0900

    add basic unit tc

--- TradingSystem/main.cpp
+++ TradingSystem/main.cpp
@@ -3,6 +3,7 @@
 #include "NemoAPI.cpp"
 #include "KiwerAPI.cpp"
 using namespace std;
+using namespace testing;
 class StockerBrockerInterface {
 public:
 	virtual void selectStockBrocker(bool IsNemo) = 0;
@@ -34,28 +35,47 @@ public:
 
 	}
 	int getPrice(string stockCode, int minute) {
-
+		return 0;
 	}
 private:
 	StockerBrockerInterface* m_API;
 };
-
-TEST(Trading, LoginTest) {
-	// Login
+class TradingFixture : public Test {
+public:
 	MockDriver Mock;
+	StockerBrockerDriver Driver{ &Mock };
+};
+TEST_F(TradingFixture, LoginTest) {
+	// Login
 	EXPECT_CALL(Mock, login("shoo-song", "1234"))
 		.Times(1);
-	StockerBrockerDriver Driver{ &Mock };
 	Driver.login("shoo-song", "1234");
 }
-TEST(Trading, BuyTest) {
-	// Buy
+TEST_F(TradingFixture, BuyTest_없는종목) {
+	EXPECT_THROW(Driver.buy("Invalid Code", 1000, 10), std::exception);
 }
-TEST(Trading, SellTest) {
-	// Sell
+TEST_F(TradingFixture, BuyTest_가격0원) {
+	EXPECT_THROW(Driver.buy("ABCD", 0, 10), std::exception);
 }
-TEST(Trading, getPrice) {
-	// getPrice
+TEST_F(TradingFixture, BuyTest_한도수량초과) {
+	EXPECT_THROW(Driver.buy("ABCD", 1000, 10000000000), std::exception);
+}
+TEST_F(TradingFixture, SellTest_정상동작) {
+	Driver.buy("ABCD", 1000, 10);
+	EXPECT_NO_THROW(Driver.sell("ABCD", 1000, 10), std::exception);
+}
+TEST_F(TradingFixture, SellTest_보유하지않은종목) {
+	EXPECT_THROW(Driver.sell("ABCD", 1000, 10), std::exception);
+}
+TEST_F(TradingFixture, SellTest_보유량초과) {
+	Driver.buy("ABCD", 1000, 10);
+	EXPECT_THROW(Driver.sell("ABCD", 1000, 20), std::exception);
+}
+TEST_F(TradingFixture, getPrice_정상동작) {
+	EXPECT_NO_THROW(Driver.getPrice("ABCD", 10), std::exception);
+}
+TEST_F(TradingFixture, getPrice_없는종목) {
+	EXPECT_THROW(Driver.getPrice("Invalid Code", 10), std::exception);
 }
 int main() {
 	::testing::InitGoogleMock();
