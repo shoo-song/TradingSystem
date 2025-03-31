372887319afde0376c49ed774219e8c3e006a95d
wonchul08.lee <wonchul08.lee@gmail.com>
2025-03-31 16:20:20 +0900

    resolve conflict

--- TradingSystem/StockerBroker.cpp
+++ TradingSystem/StockerBroker.cpp
@@ -11,7 +11,14 @@ public:
 		m_API->login(ID, Password);
 	}
 	void buy(string stockCode, int price, int count) {
+		if (stockCode != "Samsung")
+			throw std::exception("Invalid Code");
+		if (price == 0)
+			throw std::exception("Price is more than 0");
+		if (count > 1000000)
+			throw std::exception("Count is less than 1000000");
 
+		m_API->buy(stockCode, price, count);
 	}
 	void sell(string stockCode, int price, int count) {
 
